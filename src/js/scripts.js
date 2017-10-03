8a24c03265f3cf6e5573e08d847796de49117290
Alexander Järnehall <jarnehall@users.noreply.github.com>
2017-10-03 13:12:24 +0200

    Change master branch to only contain built files to make hosting via github pages possible

--- src/js/scripts.js
+++
@@ -1,7 +0,0 @@
-const ScrollReveal = require('scrollreveal');
-const SmoothScroll = require('smooth-scroll');
-const styles = require('../scss/style.scss');
-
-window.sr = ScrollReveal({ duration: 600, easing: 'ease-out', viewFactor: .3 });
-sr.reveal('.animate');
-new SmoothScroll('a[href*="#"]');
